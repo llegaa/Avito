0adeae94fa4e321698e93f2ee4a7a58058db604c
llegaa <elenagavrilova234@gmail.com>
2024-04-06 15:22:27 +0300

    first commit

---
+++ webpack.config.ts
@@ -0,0 +1,26 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = (env)=>{
+    return {
+        mode: env.mode ?? 'development',
+        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: '[name].[contenthash].js',
+            clean: true
+        },
+        plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') })],
+    }
+};
